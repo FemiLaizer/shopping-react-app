f16b5dd4d609570c3ef93da98829b807bb9a4ea5
Femi Laizer <femilaizer@gmail.com>
2023-04-24 11:24:48 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <h1>Hello from App!</h1>
+    </>
+  );
+}
+
+export default App;
